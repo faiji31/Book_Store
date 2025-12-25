f05f29ee814252e856a513c493ae6876c07cc7f4
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-25 11:43:22 +0600

    12th commit

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -17,9 +17,9 @@ const Navbar = () => {
       <ul
         tabIndex="-1"
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Item 1</a></li>
-        
-        <li><a>Item 2</a></li>
+        {
+          links
+        }
       </ul>
     </div>
     <a className="btn btn-ghost text-xl">Book Store</a>
--- src/pages/BookDetails/BookDetails.jsx
+++ src/pages/BookDetails/BookDetails.jsx
@@ -12,8 +12,13 @@ const BookDetails = () => {
            <div className='bg-gray-300 p-20 rounded'>
              <img className='size-140 w-auto' src={image} alt="" />
            </div>
-            <div className='bg-gray-300 items-center p-20'>
+            <div className='bg-gray-300 items-center p-20 w-[600px] rounded'>
                 <h2 className='text-black text-6xl font-bold'>{bookName}</h2>
+                <div className=' '>
+                    <p className='text-black text-1xl mt-5  font-bold'>Author Name: {author}</p>
+                    <p className='text-black '>Category: {category}</p>
+                       <p className='text-black '>Rating: {rating}</p>
+                </div>
             </div>
             
         </div>
